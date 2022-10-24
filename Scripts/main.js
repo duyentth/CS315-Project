fcf0b22ca1bcec12efba897a2ea6c2f24f8f51c7
Sharada Khatiwada <saradankhatiwada@gmail.com>
2022-10-24 16:28:52 -0500

    changed few css

--- Scripts/main.js
+++ Scripts/main.js
@@ -12,9 +12,22 @@ window.onload = function () {
 };
 
 function appendBestDeals() {
-  let getBestDeals = getBestDealsItems();
+  let bestDealsItems = getBestDealsItems();
   let bestDealsDiv = document.getElementById("best-deals-items-holder");
   let active = "active";
+  if (bestDealsItems.length === 0) {
+    bestDealsDiv.insertAdjacentHTML(
+      "beforeend",
+      `<div class="carousel-item ${active}">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./images/content1.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>`
+    );
+  }
   for (let bestDealItem of getBestDeals) {
     bestDealsDiv.insertAdjacentHTML(
       "beforeend",
@@ -31,11 +44,26 @@ function appendBestDeals() {
   }
 }
 function appendNewArrival() {
-  let getNewArrivals = getNewArrivalsItems();
-  let getNewArrivalsById = document.getElementById("new-arrivals-items-holder");
+  let newArrivalsItems = getNewArrivalsItems();
+  let newArrivalsItemsDiv = document.getElementById(
+    "new-arrivals-items-holder"
+  );
   let active = "active";
+  if (newArrivalsItems.length === 0) {
+    newArrivalsItemsDiv.insertAdjacentHTML(
+      "beforeend",
+      `<div class="carousel-item ${active}">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./images/content2.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>`
+    );
+  }
   for (let newArrival of getNewArrivals) {
-    getNewArrivalsById.insertAdjacentHTML(
+    newArrivalsItemsDiv.insertAdjacentHTML(
       "beforeend",
       `<div class="carousel-item ${active}">
         <div class="card" style="width: 18rem;">
